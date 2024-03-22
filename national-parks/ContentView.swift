c5cbf85b8bf73012d560b810195cba125a5e964b
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-03-22 00:34:57 -0400

    step 3 done

--- national-parks/ContentView.swift
+++ national-parks/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var parks: [Park] = []
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,7 +18,47 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear(perform: {
+            Task {
+                await fetchParks()
+                }
+        })
     }
+    
+    private func fetchParks() async {
+
+            print("On appear...")
+            // Create a Task instance
+                
+                    // URL for the API endpoint
+                    // 👋👋👋 Make sure to replace {YOUR_API_KEY} in the URL with your actual NPS API Key
+                    // Pass in any state code you like for the stateCode parameter. For instance, stateCode=fl (Florida)
+                    let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=wa&api_key=2QiL58A0mnnxiCSBEoQUiHulKxJDteJ2vX2o6z8B")!
+                    
+                    // Wrap in do/catch since URLSession async can throw errors
+                    do {
+                
+                        // Perform an asynchronous data request using URLSession
+                        let (data, _) = try await URLSession.shared.data(from: url)
+                        
+                        // Decode json data into ParksResponse type
+                                    let parksResponse = try JSONDecoder().decode(ParksResponse.self, from: data)
+
+                                    // Get the array of parks from the response
+                                    let parks = parksResponse.data
+
+                                    // Print the full name of each park in the array
+                                    for park in parks {
+                                        print(park.fullName)
+                                    }
+
+                        // TODO: Set the parks state property
+                        self.parks = parks
+                        
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+        }
 }
 
 #Preview {
---
+++ national-parks/ParksResponse.swift
@@ -0,0 +1,28 @@
+//
+//  ParksResponse.swift
+//  national-parks
+//
+//  Created by Paige Thompson on 3/22/24.
+//
+
+import Foundation
+
+struct ParksResponse: Codable {
+    let data: [Park]
+}
+
+struct Park: Codable {
+    let id: String
+    let fullName: String
+    let description: String
+    let latitude: String
+    let longitude: String
+    let images: [ParkImage]
+    let name: String
+}
+
+struct ParkImage: Codable {
+    let title: String
+    let caption: String
+    let url: String
+}
