12767656ff6af96d7602304d0922b883e9683242
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-03-22 00:19:52 -0400

    Initial Commit

---
+++ national-parks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  national-parks
+//
+//  Created by Paige Thompson on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ national-parks/national_parksApp.swift
@@ -0,0 +1,17 @@
+//
+//  national_parksApp.swift
+//  national-parks
+//
+//  Created by Paige Thompson on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct national_parksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
