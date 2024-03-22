3d53750c37c190407e7f1bbdebf071bc3dcffd16
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-03-22 00:46:55 -0400

    step 6 done

--- national-parks/ContentView.swift
+++ national-parks/ContentView.swift
@@ -14,8 +14,7 @@ struct ContentView: View {
         ScrollView {
             LazyVStack {
                 ForEach(parks) { park in
-                    Text(park.name)
-                        .font(.title)
+                    ParkRow(park: park)
                 }
             }
         }
@@ -64,4 +63,41 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ParkRow: View {
+    let park: Park
+
+    var body: some View {
+        
+        // Park row
+        Rectangle()
+            .aspectRatio(4/3, contentMode: .fit)
+            .overlay {
+                // TODO: Get image url
+                let image = park.images.first
+                let urlString = image?.url
+                let url = urlString.flatMap { string in
+                    URL(string: string)
+                }
+                
+                // TODO: Add AsyncImage
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    Color(.systemGray4)
+                }
+            }
+            .overlay(alignment: .bottomLeading) {
+                Text(park.name)
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding()
+            }
+            .cornerRadius(16)
+            .padding(.horizontal)
+    }
 }
